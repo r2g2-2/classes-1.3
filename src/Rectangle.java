13179dc5ebe5bb615662240900ca2f0b4b700a3d
r2g2 <r2g2@outlook.co.il>
2022-04-22 19:36:23 +0300

    Initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class student {
+    private int color;
+    private int length;
+    private int width;
+    public Rectangle
+
+    public int getColor() {
+        return color;
+    }
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public int getLength() {
+        return length;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    //צור משתנים צבע אורך ורוחב
+
+
+    //פעולה שיוצרת מלבן חדש
+    //getters and setters
+    //פעולה שמקבלת את שלושת המלבנים ומחזירה את השטח של כל המלבנים
+    //יצירת משתנה סכום שטח
+    //אורך כפול הרוחב של כל המלבנים והכנסת המספר למשתנה של סכום השטח
+    //החזרת משתנה סכום השטח
+    //פעולה שמקבלת את שלושת המלבנים ומחזירה את צבע המלבן עם השטח הכי גדול
+    //החזרת הצבע של המלבן עם השטח הכי גדול
+    //פעולה שמבקשת צבע ומשנה את הפרמטר של הצבע של כל המלבנים לצבע זה
+
+}
